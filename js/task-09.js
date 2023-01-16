0f912b67c1a6c2ec6c2439f696bde77f12014158
andriitomilov <andriitomilov@ukr.net>
2023-01-16 19:31:39 +0200

    done task #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  btn: document.querySelector(".change-color"),
+  text: document.querySelector(".color"),
+};
+
+refs.btn.addEventListener("click", onChangeBackgroundColor);
+
+function onChangeBackgroundColor() {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  refs.text.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
