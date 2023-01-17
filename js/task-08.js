84561d880fc216eee6c84379d46bfd0dfc11a04a
andriitomilov <andriitomilov@ukr.net>
2023-01-17 16:27:23 +0200

    done task #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector(".login-form");
 
+// console.log(refs.form);
+
+// console.log(refs.submitBtn);
+
+// console.log(refs.inputList);
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  if (formElements.email.value === "" && formElements.password.value === "") {
+    alert("Enter email and password");
+  } else if (formElements.email.value === "") {
+    alert("Enter email");
+  } else if (formElements.password.value === "") {
+    alert("Enter password");
+  } else {
+    const dataForm = new FormData(event.currentTarget);
+    const dataToServer = {};
+
+    dataForm.forEach((value, name) => (dataToServer[name] = value));
+    console.log(dataToServer);
+
+    form.reset();
+  }
+}
